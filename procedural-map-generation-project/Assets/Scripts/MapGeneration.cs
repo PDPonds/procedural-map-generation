d2bf31de776b7260ee3e39d7e4ed25abf30e7951
Po2s <pondandmeen@gmail.com>
2026-02-01 19:22:59 +0700

    Water Shader

--- procedural-map-generation-project/Assets/Scripts/MapGeneration.cs
+++ procedural-map-generation-project/Assets/Scripts/MapGeneration.cs
@@ -6,27 +6,35 @@ public class MapGeneration : MonoBehaviour
 {
     Cell[,] grid;
     float[,] noiseMap;
-
+    [Header("----- Map -----")]
     [Range(1, 128)]
     [SerializeField] int mapSize;
-
     [SerializeField] float noiseMap_Scale;
-
     [Range(0f, 1f)]
     [SerializeField] float water_Thrashold;
-
     [Range(0f, 1f)]
     [SerializeField] float sand_Thrashold;
-
     [SerializeField] GameObject grass_Prefab;
     [SerializeField] GameObject sand_Prefab;
+    [Header("----- Decorate -----")]
+    [SerializeField] float decorateNoiseScale;
+    [SerializeField] Decorate[] decoratePrefab;
+    [SerializeField] float decorate_density;
+
+
     private void Start()
     {
         GenerateMap();
-        GenerateMapVisual();
     }
 
     void GenerateMap()
+    {
+        GenerateCell();
+        GenerateMapVisual();
+        GenerateDecoration();
+    }
+
+    void GenerateCell()
     {
         grid = new Cell[mapSize, mapSize];
         noiseMap = new float[mapSize, mapSize];
@@ -83,15 +91,56 @@ public class MapGeneration : MonoBehaviour
         }
     }
 
+    void GenerateDecoration()
+    {
+        float[,] decorateNoiseMap = new float[mapSize, mapSize];
+        float xOffset = UnityEngine.Random.Range(-10000f, 10000f);
+        float yOffset = UnityEngine.Random.Range(-10000f, 10000f);
+
+        for (int y = 0; y < mapSize; y++)
+        {
+            for (int x = 0; x < mapSize; x++)
+            {
+                float noiseValue = Mathf.PerlinNoise(x * decorateNoiseScale + xOffset, y * decorateNoiseScale + yOffset);
+                decorateNoiseMap[x, y] = noiseValue;
+            }
+        }
+
+        for (int y = 0; y < mapSize; y++)
+        {
+            for (int x = 0; x < mapSize; x++)
+            {
+                Cell cell = grid[x, y];
+                if (!cell.isWater)
+                {
+                    float v = UnityEngine.Random.Range(0, decorate_density);
+                    if (decorateNoiseMap[x, y] < v)
+                    {
+                        GameObject prefab = GetPrefabFormDecorate();
+                        if (prefab != null)
+                        {
+                            Vector3 pos = new Vector3(x, 0, y);
+                            GameObject go = Instantiate(prefab, pos, Quaternion.identity);
+                            go.transform.SetParent(transform, true);
+                            go.transform.rotation = Quaternion.Euler(0, UnityEngine.Random.Range(0, 360f), 0);
+                            go.transform.localScale = Vector3.one * UnityEngine.Random.Range(0.8f, 1.2f);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
     GameObject SelectMapVisualPrefab(float value)
     {
-        if(value < water_Thrashold)
+        if (value < water_Thrashold)
         {
             return null;
         }
         else
         {
-            if(value > water_Thrashold && value <= sand_Thrashold)
+            if (value > water_Thrashold && value <= sand_Thrashold)
             {
                 return sand_Prefab;
             }
@@ -101,6 +150,35 @@ public class MapGeneration : MonoBehaviour
             }
         }
     }
+
+    GameObject GetPrefabFormDecorate()
+    {
+        float chanceSum = 0;
+        for (int i = 0; i < decoratePrefab.Length; i++)
+        {
+            chanceSum += decoratePrefab[i].dropRate;
+        }
+        
+        float rand = UnityEngine.Random.Range(0, chanceSum);
+        float runningRate = 0;
+        for (int i = 0; i < decoratePrefab.Length; i++)
+        {
+            runningRate += decoratePrefab[i].dropRate;
+            if (rand < runningRate)
+            {
+                return decoratePrefab[i].prefab;
+            }
+        }
+        return null;
+    }
+
+}
+
+[Serializable]
+public class Decorate
+{
+    public GameObject prefab;
+    public float dropRate;
 }
 
 [Serializable]
